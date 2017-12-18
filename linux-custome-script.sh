cf76983b2332f589ae9589bdad2075971b513235
M-priyanka <31058016+M-priyanka@users.noreply.github.com>
2017-12-18 18:59:28 +0530

    Update linux-custome-script.sh

--- linux-custome-script.sh
+++ linux-custome-script.sh
@@ -1,7 +1,6 @@
-n=$1
-read n
-s=0
-for(($i=0;$i<=n;$i++))
-s=`expr $s + $i`
-done
-echo "result is $s"
+echo cd ~
+echo users/who/whoami >> /tmp/script
+echo "today is : $(date)" >> /tmp/script
+echo "the no.of.users logged in as $(who -q)" >> /tmp/script
+echo "the terminal: ($UID)" >> /tmp/script
+
