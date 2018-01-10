50b2ba0a2329c219ce280f46fd03121a855dd61a
M-priyanka <31058016+M-priyanka@users.noreply.github.com>
2018-01-10 16:06:15 +0530

    Update sample-script.sh

--- sample-script.sh
+++ sample-script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/yougandar/oci-hpe/master/newscript.sh
+echo "this is new script" >> /tmp/file.txt
+echo "dwnld start" >> /tmp/file.txt
+wget https://raw.githubusercontent.com/yougandar/oci-hpe/master/newscript.sh >> /tmp/file.txt
+echo "dwnld end " >> /tmp/file.txt
