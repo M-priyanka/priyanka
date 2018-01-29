286b426c14d9ffb72525a6fc1265a0dec1e945eb
M-priyanka <31058016+M-priyanka@users.noreply.github.com>
2018-01-29 15:45:07 +0530

    Update suse-script.sh

--- suse-script.sh
+++ suse-script.sh
@@ -5,17 +5,17 @@ sudo mkdir /tmp/azurefiles
 
 ## Restricted Environment Setup
 sudo cp /bin/bash /bin/rbash 
-sudo usermod -s /bin/rbash suseser
-sudo mkdir  -p /home/suseuser/programs
+sudo usermod -s /bin/rbash suse
+sudo mkdir  -p /home/suse/programs
 sudo wget -O /tmp/azurefiles/bashprofile.txt https://s3.amazonaws.com/verticatestdrive/PredictiveMaint/bashprofile.txt
 
-sudo cat /tmp/azurefiles/bashprofile.txt | dos2unix | sudo tee /home/suseuser/.bash_profile
+sudo cat /tmp/azurefiles/bashprofile.txt | dos2unix | sudo tee /home/suse/.bash_profile
 
 #sudo ln -s /opt/puppetlabs/bin/puppet /home/azureuser/programs/
-sudo ln -s /bin/ls /home/suseuser/programs/
-sudo ln -s /usr/bin/cd /home/suseuser/programs/
-sudo ln -s /usr/bin/ssh /home/suseuser/programs/
+sudo ln -s /bin/ls /home/suse/programs/
+sudo ln -s /usr/bin/cd /home/suse/programs/
+sudo ln -s /usr/bin/ssh /home/suse/programs/
 
 
-sudo chattr +i /home/suseuser/.bash_profile
+sudo chattr +i /home/suse/.bash_profile
 
